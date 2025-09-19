5332dd5b02e95cacf3f21ddd791dd38fd49975b2
JocManueh <Jose_manuel.diaz@uao.edu.co>
2025-09-19 18:13:11 -0500

    Add END scene and update player animation transitions
    Added a new END.unity scene with UI elements and camera setup. Updated PLAYERDEF.controller to include new animation parameters and transitions for jumping and movement. Adjusted object positions, camera settings, and tilemap data in ESCENA1NEW.unity. Updated TagManager and made minor changes in ESCENA2.unity and PlayerMove.cs to support these features.

--- GAME2D/Assets/Scripts/PlayerMove.cs
+++ GAME2D/Assets/Scripts/PlayerMove.cs
@@ -5,14 +5,16 @@ public class PlayerMove : MonoBehaviour
 
     private Rigidbody2D _Rigidbody2D;
     public float horizontal;
-    public float vertical;
-    public float speed = 5f;
+    public float speed = 1f;
+    public float jumpForce = 3f;
+    private bool isGrounded;
 
     public Animator animator; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
 
     }
 
@@ -24,8 +26,29 @@ public class PlayerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
-        animator.SetFloat("Movement", horizontal);
+        animator.SetFloat("Movement", horizontal*speed);
         _Rigidbody2D.linearVelocity = new Vector2(horizontal * speed, _Rigidbody2D.linearVelocity.y);
+
+        if (horizontal > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontal < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            _Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            animator.SetBool("isJumping", false);
+        }
     }
 }
-
+    
